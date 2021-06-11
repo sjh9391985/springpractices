8375be1e369b1ae5f49971e9b48253ac442e9f8a
BIT <BIT@DESKTOP-DSRR2KB>
2021-06-11 12:50:16 +0900

    wiring #2

---
+++ container/src/main/java/com/douzone/container/user/User1.java
@@ -0,0 +1,12 @@
+package com.douzone.container.user;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User1 {
+	
+	public String getName() {
+		return "손재현";
+	}
+	
+}

c083f62b31137f9961b95aca6eeb3ec9ef6302bd
JaeHyeon Son <BIT@DESKTOP-DSRR2KB>
2021-06-14 15:47:57 +0900

    explicit wiring - xml-#3

---
+++ container/src/main/java/com/douzone/container/config/videosystem/mixing/DVDConfig.java
@@ -0,0 +1,28 @@
+package com.douzone.container.config.videosystem.mixing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.douzone.container.videosystem.Avengers;
+import com.douzone.container.videosystem.BlankDisc;
+import com.douzone.container.videosystem.DigitalVideoDisc;
+
+@Configuration
+public class DVDConfig {
+	
+	@Bean
+	public DigitalVideoDisc avengers() {
+		return new Avengers();
+	}
+	
+	@Bean(name="AvengersInfinityWar")
+	public DigitalVideoDisc blankDisc() {
+		BlankDisc blankDisc = new BlankDisc();
+		
+		blankDisc.setTitle("Avengers Infinity War");
+		blankDisc.setStudio("MARVLE");	
+		return blankDisc;
+	}
+	
+	
+}
--- container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerXmlConfigTest.java
+++ container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerXmlConfigTest.java
@@ -1,5 +1,6 @@
 package com.douzone.container.config.videosystem;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Ignore;
@@ -12,34 +13,168 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.douzone.container.videosystem.DVDPack;
+import com.douzone.container.videosystem.DVDPlayer;
 import com.douzone.container.videosystem.DigitalVideoDisc;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/com/douzone/container/config/videosystem/DVDPlayerConfig.xml"})
+@ContextConfiguration(locations = {"classpath:com/douzone/container/config/videosystem/DVDPlayerConfig.xml"})
 public class DVDPlayerXmlConfigTest {
 	@Rule
 	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
-	
-	//@Autowired
+
+	// @Autowired
 	// 예외 발생
-	// XML bean 설정 시 id는 자동으로 부여 되지 않음.
-	// @Qualifier 사용불가
-	DigitalVideoDisc dvd1;
-	
+	// XML Bean 설정 시 id는 자동으로 부여 되지 않는다.
+	// @Qualifier 사용 불가
+	private DigitalVideoDisc dvd1;
+
 	@Autowired
 	@Qualifier("ironMan")
-	DigitalVideoDisc dvd2;
+	private DigitalVideoDisc dvd2;
+
+	@Autowired
+	@Qualifier("avengersInfinityWar")
+	private DigitalVideoDisc dvd3;
+
+	@Autowired
+	@Qualifier("avengersEndgame")
+	private DigitalVideoDisc dvd4;
+
+	@Autowired
+	@Qualifier("avengersAgeOfUltron")
+	private DigitalVideoDisc dvd5;
+
+	@Autowired
+	@Qualifier("captainAmerica")
+	private DigitalVideoDisc dvd6;
+
+	@Autowired
+	@Qualifier("avengersDirectorEdition")
+	private DigitalVideoDisc dvd7;
+
+	@Autowired
+	@Qualifier("avengersExpansionPack1")
+	private DigitalVideoDisc dvd8;
+
+	@Autowired
+	@Qualifier("avengersExpansionPack2")
+	private DigitalVideoDisc dvd9;
+
+	@Autowired
+	@Qualifier("avengersExpansionPack3")
+	private DigitalVideoDisc dvd10;
+
+	@Autowired
+	@Qualifier("avengersTriplepack")
+	private DVDPack dvdPack;
 	
-	@Ignore // 테스트 생략을 표시
+	@Autowired
+	@Qualifier("dvdPlayer1")
+	private DVDPlayer player1;
+	
+	@Autowired
+	@Qualifier("dvdPlayer2")
+	private DVDPlayer player2;
+	
+	@Autowired
+	@Qualifier("dvdPlayer3")
+	private DVDPlayer player3;
+	
+	@Autowired
+	@Qualifier("dvdPlayer4")
+	private DVDPlayer player4;
+	
+	@Autowired
+	@Qualifier("dvdPlayer5")
+	private DVDPlayer player5;
+	
+	
+	
+	@Ignore
 	@Test
 	public void testDVD1() {
-		assertNotNull(dvd1);	
+		assertNotNull(dvd1);
 	}
 	
 	@Test
 	public void testDVD2() {
-		assertNotNull(dvd2);	
+		assertNotNull(dvd2);
+	}
+
+	@Test
+	public void testDVD3() {
+		assertNotNull(dvd3);
+	}
+
+	@Test
+	public void testDVD4() {
+		assertNotNull(dvd4);
+	}
+
+	@Test
+	public void testDVD5() {
+		assertNotNull(dvd5);
+	}
+
+	@Test
+	public void testDVD6() {
+		assertNotNull(dvd6);
+	}
+
+	@Test
+	public void testDVD7() {
+		assertNotNull(dvd7);
+	}
+
+	@Test
+	public void testDVD8() {
+		assertNotNull(dvd8);
+	}
+
+	@Test
+	public void testDVD9() {
+		assertNotNull(dvd9);
+	}
+
+	@Test
+	public void testDVD10() {
+		assertNotNull(dvd10);
 	}
 	
+	@Test
+	public void testDVDPack() {
+		assertNotNull(dvdPack);
+	}
+	
+	@Test
+	public void testPlay1() {
+		player1.play();
+		assertEquals("playing Movie MARVEL's IronMan", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	}
+	
+	@Test
+	public void testPlay2() {
+		player2.play();
+		assertEquals("playing Movie MARVEL's IronMan", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	}
+	
+	@Test
+	public void testPlay3() {
+		player3.play();
+		assertEquals("playing Movie MARVEL's IronMan", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	}
+	
+	@Test
+	public void testPlay4() {
+		player4.play();
+		assertEquals("Playing Movie MARVEL's Avengers Director Edition", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	}
+	
+	@Test
+	public void testPlay5() {
+		player5.play();
+		assertEquals("Playing Movie MARVEL's Avengers Director Edition", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	}
 	
 }
